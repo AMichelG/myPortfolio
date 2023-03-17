a1642f11e88b6d0803c5109b3e4b35187c68427d
Antonio Michel <antoniomichelgonzalez@gmail.com>
2023-03-17 14:33:00 -0600

    fixed css

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./about.css";
+import webdev from "../../assets/webdev.png";
 import portrait from "../../assets/portrait2.png";
 import { FiAward } from "react-icons/fi";
 
@@ -12,12 +13,12 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={portrait} alt="me" />
+            <img src={webdev} alt="me" />
           </div>
         </div>
 
         <div className="about__content">
-          <div className="about__cards">
+          {/* <div className="about__cards">
             <article className="about__card">
               <FiAward className="about__icon" />
               <h5>Experience</h5>
@@ -33,12 +34,20 @@ const About = () => {
               <h5>Experience</h5>
               <small>1+ Year Coding</small>
             </article>
-          </div>
+          </div> */}
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-            quia quaerat voluptatum doloribus doloremque quos nemo eaque nulla
-            suscipit nam rem facere, ipsa in deleniti architecto autem sapiente
-            aliquid ab.
+            Hi, I'm Antonio, a former Energy Engineer who switched careers into
+            Web Development.
+            <br />
+            <br />
+            I started learning to code by looking up tutorials and courses
+            online, and eventually got into the Soy Henry Full Stack Web
+            Development Bootcamp where I learned technologies such as
+            JavaScript, React, Node.js and others.
+            <br />
+            <br />
+            Currently I'm starting to learn Android Development while also
+            practising my previously learned skills.
           </p>
           <a href="#contact" className="btn btn-primary">
             Let's talk
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -15,28 +15,6 @@ const Experience = () => {
       <div className="container experience__container">
         <article className="experience__item">
           <div className="experience__item-image">
-            <img src={eCommerce} />
-          </div>
-          <h3>Next.js E-Commerce</h3>
-          <div className="experience__item-cta">
-            <a
-              href="https://github.com/AMichelG/Tech-e.commerce"
-              className="btn"
-              target="_blank"
-            >
-              Github Repo
-            </a>
-            <a
-              href="https://tech-e-commerce.vercel.app/"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="experience__item">
-          <div className="experience__item-image">
             <img src={Pokedex1} />
           </div>
           <h3>React-Redux Pokedex</h3>
@@ -50,6 +28,28 @@ const Experience = () => {
             </a>
             <a
               href="https://react-pokedex-amichelg.vercel.app/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="experience__item">
+          <div className="experience__item-image">
+            <img src={eCommerce} />
+          </div>
+          <h3>Next.js E-Commerce</h3>
+          <div className="experience__item-cta">
+            <a
+              href="https://github.com/AMichelG/Tech-e.commerce"
+              className="btn"
+              target="_blank"
+            >
+              Github Repo
+            </a>
+            <a
+              href="https://tech-e-commerce.vercel.app/"
               className="btn btn-primary"
               target="_blank"
             >
